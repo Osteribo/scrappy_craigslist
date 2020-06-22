1338d894113d6ab9abaef53566bdc9e1fa5060e2
ALO <36780370+Osteribo@users.noreply.github.com>
2020-06-22 16:19:29 -0700

    Update: created search page and search form

--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    return render(request, 'my_app/new_search.html')
