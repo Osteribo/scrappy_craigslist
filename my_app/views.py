97b789942f281cb98bd818ca38130f04d63fd928
ALO <36780370+Osteribo@users.noreply.github.com>
2020-06-24 22:19:00 -0700

    Update: searches now include photos

--- my_app/views.py
+++ my_app/views.py
@@ -9,6 +9,8 @@ from . import models
 
 BASE_CRAIGSLIST_URL = "https://seattle.craigslist.org/search/?query={}"
 
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 
 def home(request):
     return render(request, 'base.html')
@@ -47,8 +49,18 @@ def new_search(request):
         else:
             post_price = 'N/A'
 
-        final_postings.append((post_title, post_url, post_price))
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            print(post_image_id)
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        
+        else:
+            post_image_url = "https://craigslist.org/images/peace.jpg"
 
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    
     stuff_for_frontend = {
         'search': search,
         'final_postings': final_postings,
